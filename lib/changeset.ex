697f865e442531b59577c2cdef72e778f8df9d64
Erich Grunewald <erichgrunewald@gmail.com>
2016-03-07 12:21:07 +0100

    Implement naïve edit distance algorithm.

--- lib/changeset.ex
+++ lib/changeset.ex
@@ -6,9 +6,7 @@ defmodule Changeset do
   in list A.
   """
   @spec difference([], []) :: {[], []}
-  def difference(source, target) do
-    difference(source, target, {[], []})
-  end
+  def difference(source, target), do: diff(source, target, {[], []})
 
   defp diff([], [], result), do: result
   defp diff([], r, {l_diff, r_diff}), do: {l_diff, r_diff ++ r}
@@ -24,11 +22,35 @@ defmodule Changeset do
   end
 
   @doc """
-  Calculate the edit distance between two lists, i.e. the minimal steps required
-  to go from list A to list B.
+  Calculate the the minimal steps (insertions, deletions, substitutions and
+  moves) required to turn list A into list B.
   """
-  @spec distance([], []) :: [{atom, any, non_neg_integer}]
-  def distance(source, target) do
+  @spec edit_steps([], []) :: [{atom, any, non_neg_integer}]
+  def edit_steps(source, target), do: steps(source, target, [])
+
+  defp steps(source, target, result) do
     []
+  end
+
+
+  @doc """
+  Calculate the the edit distance between two lists, i.e. how many insertions,
+  deletions or substitutions are required to turn list A into list B.
+  """
+  @spec distance([], []) :: non_neg_integer
+  def distance(source, target), do: dist(source, target, Enum.count(source), Enum.count(target))
+
+  defp dist(_source, _target, i, 0), do: i
+  defp dist(_source, _target, 0, j), do: j
+  defp dist(source, target, i, j) do
+    if Enum.fetch!(source, i - 1) == Enum.fetch!(target, j - 1) do
+      dist(source, target, i - 1, j - 1)
+    else
+      Enum.min([
+        dist(source, target, i - 1, j) + 1,
+        dist(source, target, i, j - 1) + 1,
+        dist(source, target, i - 1, j - 1) + 1
+        ])
+    end
   end
 end
--- test/changeset_test.exs
+++ test/changeset_test.exs
@@ -14,8 +14,8 @@ defmodule ChangesetTest do
     assert r == ["Heidegger", "Schopenhauer"]
   end
 
-  test "edit distance", %{source: source, target: target} do
-    edits = Changeset.distance(source, target)
+  test "edit steps", %{source: source, target: target} do
+    edits = Changeset.edit_steps(source, target)
     assert edits == [
       {:insertion, "Heidegger", 0},
       {:substitution, "Schopenhauer", 3},
@@ -23,12 +23,17 @@ defmodule ChangesetTest do
     ]
   end
 
-  test "reverse edit distance", %{source: source, target: target} do
-    edits = Changeset.distance(target, source)
+  test "reverse edit steps", %{source: source, target: target} do
+    edits = Changeset.edit_steps(target, source)
     assert edits == [
       {:deletion, "Heidegger", 0},
       {:substitution, "Wittgenstein", 2},
       {:insertion, "Proudhon", 4}
     ]
+  end
+
+  test "edit distance", %{source: source, target: target} do
+    assert Changeset.distance(source, target) == 3
+    assert Changeset.distance(target, source) == 3
   end
 end
