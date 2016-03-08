278399a752efa90d095f0a4dc69e26be3d9a2c9c
Erich Grunewald <erichgrunewald@gmail.com>
2016-03-08 11:15:51 +0100

    Add calculation of edit steps.

--- lib/changeset.ex
+++ lib/changeset.ex
@@ -26,30 +26,62 @@ defmodule Changeset do
   moves) required to turn list A into list B.
   """
   @spec edit_steps([], []) :: [{atom, any, non_neg_integer}]
-  def edit_steps(source, target), do: steps(source, target, [])
-
-  defp steps(source, target, result) do
-    []
+  def edit_steps(source, target) do
+    {res, _} = stp(source, target, [], Enum.count(source), Enum.count(target))
+    res |> reduce_edit_steps
   end
 
+  defp stp(_src, _tgt, res, 0, 0), do: {res, 0}
+  defp stp(src, tgt, res, i, 0) do
+    {res, cost} = stp(src, tgt, mk_tup(:delete, src, i) ++ res, i - 1, 0)
+    {res, cost + 1}
+  end
+  defp stp(src, tgt, res, 0, j) do
+    {res, cost} = stp(src, tgt, mk_tup(:insert, tgt, j) ++ res, 0, j - 1)
+    {res, cost + 1}
+  end
+  defp stp(src, tgt, res, i, j) do
+    if Enum.fetch!(src, i - 1) == Enum.fetch!(tgt, j - 1) do
+      stp(src, tgt, res, i - 1, j - 1)
+    else
+      [
+        stp(src, tgt, mk_tup(:delete, src, i) ++ res, i - 1, j),
+        stp(src, tgt, mk_tup(:insert, tgt, j) ++ res, i, j - 1),
+        stp(src, tgt, mk_tup(:substitute, tgt, j) ++ res, i - 1, j - 1)
+      ]
+      |> Enum.map(fn {res, cost} -> {res, cost + 1} end)
+      |> Enum.min_by(fn {_, cost} -> cost end)
+    end
+  end
+
+  defp mk_tup(type, list, dest) do
+    [{type, Enum.fetch!(list, dest - 1), dest - 1}]
+  end
+
+  defp reduce_edit_steps(edit_steps) do
+    edit_steps
+  end
 
   @doc """
-  Calculate the the edit distance between two lists, i.e. how many insertions,
-  deletions or substitutions are required to turn list A into list B.
+  Calculate the the Levenshtein distance between two lists, i.e. how many
+  insertions, deletions or substitutions are required to turn one given list
+  into another.
   """
-  @spec distance([], []) :: non_neg_integer
-  def distance(source, target), do: dist(source, target, Enum.count(source), Enum.count(target))
+  @spec levenshtein([], []) :: non_neg_integer
+  def levenshtein(source, target) do
+    lev(source, target, Enum.count(source), Enum.count(target))
+  end
 
-  defp dist(_source, _target, i, 0), do: i
-  defp dist(_source, _target, 0, j), do: j
-  defp dist(source, target, i, j) do
+  defp lev(_source, _target, i, 0), do: i
+  defp lev(_source, _target, 0, j), do: j
+  defp lev(source, target, i, j) do
     if Enum.fetch!(source, i - 1) == Enum.fetch!(target, j - 1) do
-      dist(source, target, i - 1, j - 1)
+      lev(source, target, i - 1, j - 1)
     else
       Enum.min([
-        dist(source, target, i - 1, j) + 1,
-        dist(source, target, i, j - 1) + 1,
-        dist(source, target, i - 1, j - 1) + 1
+        lev(source, target, i - 1, j) + 1,
+        lev(source, target, i, j - 1) + 1,
+        lev(source, target, i - 1, j - 1) + 1
         ])
     end
   end
--- test/changeset_test.exs
+++ test/changeset_test.exs
@@ -3,37 +3,127 @@ defmodule ChangesetTest do
   doctest Changeset
 
   setup do
-    source = ["Kant", "Nietzsche", "Wittgenstein", "Marx", "Proudhon"]
-    target = ["Heidegger", "Kant", "Nietzsche", "Schopenhauer", "Marx"]
-    {:ok, source: source, target: target}
+    kitten = String.split("kitten", "", trim: true)
+    sitting = String.split("sitting", "", trim: true)
+
+    preterit = String.split("preterit", "", trim: true)
+    zeitgeist = String.split("zeitgeist", "", trim: true)
+
+    garvey = String.split("garvey", "", trim: true)
+    avery = String.split("avery", "", trim: true)
+
+    {:ok,
+      kitten: {kitten, sitting},
+      preterit: {preterit, zeitgeist},
+      garvey: {garvey, avery}
+    }
   end
 
-  test "difference", %{source: source, target: target} do
-    {l, r} = Changeset.difference(source, target)
-    assert l == ["Wittgenstein", "Proudhon"]
-    assert r == ["Heidegger", "Schopenhauer"]
+  test "kitten <-> sitting difference", %{kitten: {s1, s2}} do
+    {l, r} = Changeset.difference(s1, s2)
+    assert l == ["k", "e"]
+    assert r == ["s", "i", "g"]
   end
 
-  test "edit steps", %{source: source, target: target} do
-    edits = Changeset.edit_steps(source, target)
+  test "preterit <-> zeitgeist difference", %{preterit: {s1, s2}} do
+    {l, r} = Changeset.difference(s1, s2)
+    assert l == ["p", "r", "r"]
+    assert r == ["z", "g", "i", "s"]
+  end
+
+  test "garvey <-> avery difference", %{garvey: {s1, s2}} do
+    {l, r} = Changeset.difference(s1, s2)
+    assert l == ["g"]
+    assert r == []
+  end
+
+  test "kitten -> sitting edit steps", %{kitten: {s1, s2}} do
+    edits = Changeset.edit_steps(s1, s2)
     assert edits == [
-      {:insertion, "Heidegger", 0},
-      {:substitution, "Schopenhauer", 3},
-      {:deletion, nil, 5}
+      {:substitute, "s", 0},
+      {:substitute, "i", 4},
+      {:insert, "g", 6}
     ]
   end
 
-  test "reverse edit steps", %{source: source, target: target} do
-    edits = Changeset.edit_steps(target, source)
+  test "sitting -> kitten edit steps", %{kitten: {s1, s2}} do
+    edits = Changeset.edit_steps(s2, s1)
     assert edits == [
-      {:deletion, "Heidegger", 0},
-      {:substitution, "Wittgenstein", 2},
-      {:insertion, "Proudhon", 4}
+      {:substitute, "k", 0},
+      {:substitute, "e", 4},
+      {:delete, "g", 6}
     ]
   end
 
-  test "edit distance", %{source: source, target: target} do
-    assert Changeset.distance(source, target) == 3
-    assert Changeset.distance(target, source) == 3
+  test "garvey -> avery edit steps", %{garvey: {s1, s2}} do
+    edits = Changeset.edit_steps(s1, s2)
+    assert edits == [
+      {:delete, "g", 0},
+      {:delete, "r", 2},
+      {:insert, "r", 3}
+    ]
+  end
+
+  test "preterit -> zeitgeist edit steps", %{preterit: {s1, s2}} do
+    edits = Changeset.edit_steps(s1, s2)
+    assert edits == [
+      {:substitute, "z", 0},
+      {:delete, "r", 1},
+      {:insert, "i", 2},
+      {:insert, "g", 4},
+      {:delete, "r", 5},
+      {:insert, "s", 7}
+    ]
+  end
+
+  test "zeitgeist -> preterit edit steps", %{preterit: {s1, s2}} do
+    edits = Changeset.edit_steps(s2, s1)
+    assert edits == [
+      {:substitute, "p", 0},
+      {:insert, "r", 1},
+      {:delete, "i", 2},
+      {:delete, "g", 4},
+      {:insert, "r", 5},
+      {:delete, "s", 7}
+    ]
+  end
+
+  test "kitten -> empty string edit steps", %{kitten: {s1, _s2}} do
+    edits = Changeset.edit_steps(s1, [])
+    assert edits == [
+      {:delete, "k", 0},
+      {:delete, "i", 1},
+      {:delete, "t", 2},
+      {:delete, "t", 3},
+      {:delete, "e", 4},
+      {:delete, "n", 5}
+    ]
+  end
+
+  test "empty string -> kitten edit steps", %{kitten: {s1, _s2}} do
+    edits = Changeset.edit_steps([], s1)
+    assert edits == [
+      {:insert, "k", 0},
+      {:insert, "i", 1},
+      {:insert, "t", 2},
+      {:insert, "t", 3},
+      {:insert, "e", 4},
+      {:insert, "n", 5}
+    ]
+  end
+
+  test "kitten <-> sitting levenshtein distance", %{kitten: {s1, s2}} do
+    assert Changeset.levenshtein(s1, s2) == 3
+    assert Changeset.levenshtein(s2, s1) == 3
+  end
+
+  test "preterit <-> zeitgeist levenshtein distance", %{preterit: {s1, s2}} do
+    assert Changeset.levenshtein(s1, s2) == 6
+    assert Changeset.levenshtein(s2, s1) == 6
+  end
+
+  test "garvey <-> avery levenshtein distance", %{garvey: {s1, s2}} do
+    assert Changeset.levenshtein(s1, s2) == 3
+    assert Changeset.levenshtein(s2, s1) == 3
   end
 end
